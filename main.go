a1f0dc093d5291597c8e6f398202b09df656baac
MasahikoYonezawa <y.masahiko0802@gmail.com>
2022-10-20 20:10:48 +0900

    feat: 3.3 go-isatty

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/mattn/go-isatty"
 	"io"
 	"log"
 	"net/http"
@@ -12,9 +13,30 @@ import (
 	"strings"
 )
 
+type flusher interface {
+	Flush() error
+}
+
 func main() {
 	//filePath()
-	buffering()
+	//buffering()
+	useIsatty()
+}
+
+func useIsatty() {
+	var output io.Writer
+	if isatty.IsTerminal(os.Stdout.Fd()) {
+		output = os.Stdout
+	} else {
+		output = bufio.NewWriter(os.Stdout)
+	}
+
+	for i := 0; i < 100; i++ {
+		fmt.Fprintln(output, strings.Repeat("x", 100))
+	}
+	if _o, ok := output.(flusher); ok {
+		_o.Flush()
+	}
 }
 
 func buffering() {
